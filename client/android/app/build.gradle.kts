676e415e1350f21a05c073d4961dff4dc4c23f56
Pablo García Zarza <pablogarciazarza@gmail.com>
2026-08-09 16:46:20 +0200

    Se cambia el identificador de la versión.

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 11
-        versionName = "0.2.0"
+        versionCode = 12
+        versionName = "0.2.1"
     }
 
     signingConfigs {
--- client/lib/util/config_xest.dart
+++ client/lib/util/config_xest.dart
@@ -1,5 +1,5 @@
 class ConfigXest {
-  static const String version = "0.2.0";
+  static const String version = "0.2.1";
 
   static const bool development = false;
 
